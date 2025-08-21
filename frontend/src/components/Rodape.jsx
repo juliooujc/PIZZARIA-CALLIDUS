1955551a59fe334454b36a32e2880c057a1e40c3
Júlio Cleiton <jcsme.snf24@uea.edu.br>
2025-08-21 02:34:37 -0300

    Corrigido problema de marge

--- frontend/src/components/Rodape.jsx
+++ frontend/src/components/Rodape.jsx
@@ -1,19 +1,11 @@
-import React from "react"
-
 const Rodape = () => {
     return (
         <footer className="rodape">
             <p>
-<<<<<<< HEAD
-                Os direitos não são reservados &#8212; &copy; Copyright 2025
-=======
                 Conteúdo criado e adaptado por Júlio e João &#8212; &copy; Copyright 2025
->>>>>>> 92a1b7f57e6bdc65d41b7c7f66f6ca09e71e69eb
             </p>
         </footer>
-
     );
-    
 };
 
 export default Rodape;
